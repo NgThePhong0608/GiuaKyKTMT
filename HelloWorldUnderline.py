1ef54cd81248aa83e33c99b206a97b06c9440b43
NguyenThePhong <nguyenthephong_t65@hus.edu.vn>
2022-05-14 10:07:01 +0700

    Hello World underline

--- HelloWorldUnderline.py
+++ HelloWorldUnderline.py
@@ -1,6 +1,6 @@
 import time
 
-
+import RPi.GPIO as GPIO
 import Adafruit_Nokia_LCD as LCD
 from PIL import Image
 from PIL import ImageDraw
@@ -8,13 +8,15 @@ from PIL import ImageFont
 
 
 def main():
+    BT1 = 14
     SCLK = 23
     Din = 27
     DC = 17
     RST = 15
     CS = 18
     BT1 = 14
-
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(BT1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     global disp
     disp = LCD.PCD8544(DC, RST, SCLK, Din, CS)
     disp.begin(contrast=60)
@@ -30,8 +32,23 @@ def main():
     disp.image(image)
     disp.display()
 
+    while True:
+        if GPIO.input(BT1) == GPIO.LOW:
+            print('BT1 is pressed')
+            disp.clear()
+            disp1 = LCD.PCD8544(DC, RST, SCLK, Din, CS)
+            image1 = Image.new('1', (LCD.LCDWIDTH, LCD.LCDHEIGHT))
+            draw1 = ImageDraw.Draw(image1)
+            draw1.rectangle((0, 0, LCD.LCDWIDTH, LCD.LCDHEIGHT),
+                            outline=255, fill=255)
+            draw1.line((8, 30, 68, 30), fill=0)
+
+            disp1.image(image1)
+            disp1.display()
+
 
 try:
     main()
 except KeyboardInterrupt:
-    disp.clear()
+    # disp.clear()
+    pass
